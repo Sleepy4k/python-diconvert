9da6d097150d59f9946e90806658f04c72d60816
Sleepy4k <sarahpalastrin@gmail.com>
2023-02-14 15:55:15 +0700

    chore: main.py

--- main.py
+++ main.py
@@ -1,126 +1,121 @@
 # Import Modules
-from os import replace, system
+import os
+
+def clearcmd():
+    os.system('cls') if os.name == 'nt' else os.system('clear')
+
+def title():
+    print("-----------------------------")
+    print("Number Convertion with Python")
+    print("-----------------------------")
 
 def menu():
-    system('cls')
+    clearcmd()
+    title()
+    print("1. Decimal")
+    print("2. Binary")
+    print("3. Octal")
+    print("4. Hexadecimal")
+    print("5. Exit")
+    print("-----------------------------")
 
-    print('-----------------------------')
-    print('Number Convertion with Python')
-    print('-----------------------------')
-    print('1. Decimal')
-    print('2. Binary')
-    print('3. Octal')
-    print('4. Hexadecimal')
-    print('5. Exit')
-    print('-----------------------------')
+def get_input(base):
+    data = input(f"Input your {base} number : ")
+    if not data:
+        raise ValueError(f"{base} number is required")
+    return data
 
-def error(error = 'Output not available!'):
-    print('-----------------------------')
-    print(error)
-    print('-----------------------------')
-    
-    choice = input('Do you want to continue? [yes/no] : ')
+def error(err):
+    print("-----------------------------")
+    print(err)
+    print("-----------------------------")
+    choice = str(input("Do you want to continue? [yes/no] : "))
+    init() if choice.lower() in ["yes", "y"] else exit()
 
-    if choice == 'yes' or choice == 'y':
-        init()
-    elif choice == 'no' or choice == 'n':
-        exit()
-    else:
-        error()
+def convertion(base, data):
+    try:
+        if base == 1:
+            decimal = int(data)
+            binary = bin(decimal).replace("0b", "")
+            octal = oct(decimal).replace("0o","")
+            hexadecimal = hex(decimal).replace("0x","")
+        elif base == 2:
+            binary = int(data, 2)
+            decimal = int(data, 2)
+            octal = oct(decimal).replace("0o","")
+            hexadecimal = hex(decimal).replace("0x","")
+        elif base == 3:
+            octal = int(data, 8)
+            decimal = int(data, 8)
+            binary = bin(decimal).replace("0b","")
+            hexadecimal = hex(decimal).replace("0x","")
+        elif base == 4:
+            hexadecimal = data
+            decimal = int(data, 16)
+            binary = bin(decimal).replace("0b","")
+            octal = oct(decimal).replace("0o","")
+        return (decimal, binary, octal, hexadecimal)
+    except ValueError:
+        error("System has value error")
+    except TypeError:
+        error("System has data type error")
+    except Exception as err:
+        error(f"System error: {err}")
 
 def confirmation(func):
-    choice = input('Do you want to try again? [yes/no] : ')
-
-    if choice == 'yes' or choice == 'y':
-        if func == 'decimal':
-            decimal()
-        elif func == 'binary':
-            binary()
-        elif func == 'octal':
-            octal()
-        elif func == 'hexadecimal':
-            Hexadecimal()
-    elif choice == 'no' or choice == 'n':
+    choice = input("Do you want to try again? [yes/no] : ")
+    if choice.lower() in ["yes", "y"]:
+        func()
+    elif choice.lower() in ["no", "n"]:
         init()
     else:
-        error()
+        error("Invalid choice")
 
 def result(decimal, binary, octal, hexadecimal):
-    print('-----------------------------')
-    print('Decimal     : ', decimal)
-    print('Binary      : ', binary)
-    print('Octal       : '. octal)
-    print('Hexadecimal : ', hexadecimal)
-    print('-----------------------------')
+    print("-----------------------------")
+    print(f"Decimal     : {decimal}")
+    print(f"Binary      : {binary}")
+    print(f"Octal       : {octal}")
+    print(f"Hexadecimal : {hexadecimal}")
+    print("-----------------------------")
 
 def decimal():
-    try:
-        decimal = int(input('Input your decimal number : '))
-    except ValueError:
-        error('The number does not match')
-
-    binary = bin(decimal).replace('0b','')
-    octal = oct(decimal).replace('0o','')
-    hexadecimal = hex(decimal).replace('0x','')
-
+    data = get_input("decimal")
+    decimal, binary, octal, hexadecimal = convertion(1, data)
     result(decimal, binary, octal, hexadecimal)
-    error('decimal')
+    confirmation(decimal)
 
 def binary():
-    try:
-        binary = input('Input your binary number : ')
-    except ValueError:
-        error('The number does not match')
-
-    decimal = int(binary, 2)
-    octal = oct(binary).replace('0o', '')
-    hexadecimal = hex(binary).replace('0x', '')
-    
+    data = get_input("binary")
+    decimal, binary, octal, hexadecimal = conversion(2, data)
     result(decimal, binary, octal, hexadecimal)
-    error('binary')
+    confirmation(binary)
 
 def octal():
-    try:
-        binary = input('Input your octal number : ')
-    except ValueError:
-        error('The number does not match')
-
-    decimal = int(binary, 2)
-    octal = oct(binary).replace('0o', '')
-    hexadecimal = hex(binary).replace('0x', '')
-    
+    data = get_input("octal")
+    decimal, binary, octal, hexadecimal = conversion(3, data)
     result(decimal, binary, octal, hexadecimal)
-    error('octal')
+    confirmation(octal)
 
 def hexadecimal():
-    try:
-        binary = input('Input your hexadecimal number : ')
-    except ValueError:
-        error('The number does not match')
-
-    decimal = int(binary, 2)
-    octal = oct(binary).replace('0o', '')
-    hexadecimal = hex(binary).replace('0x', '')
-    
+    data = get_input("hexadecimal")
+    decimal, binary, octal, hexadecimal = conversion(4, data)
     result(decimal, binary, octal, hexadecimal)
-    error('hexadecimal')
+    confirmation(hexadecimal)
 
 def logic():
     select = input('Select your output : ')
     print('-----------------------------')
+    functions = {
+        '1': decimal,
+        '2': binary,
+        '3': octal,
+        '4': hexadecimal,
+        '5': exit
+    }
 
-    if select == '1':
-        decimal()
-    elif select == '2':
-        binary()
-    elif select == '3':
-        octal()
-    elif select == '4':
-        hexadecimal()
-    elif select == '5':
-        exit()
-    else:
-        error()
+    function = functions.get(select, error)
+    function()
 
 def init():
     menu()
