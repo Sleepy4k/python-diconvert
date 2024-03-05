cc875210a106999c083a9e48c7f29b91fb609868
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-03-05 22:58:19 +0700

    chore(config): add function to refresh current lang

--- config/__init__.py
+++ config/__init__.py
@@ -9,5 +9,26 @@ CONFIG = {
   "CMD": CMDConfig,
   "GUI": GUIConfig,
   "lang": LANG[LANGConfig["default"]] if LANGConfig["default"] in LANG else LANG["en"],
-  "output": ENUMS["ui_type"].CONSOLE # You can change this to ENUMS["ui_type"].CONSOLE or ENUMS["ui_type"].GUI
+  "output": ENUMS["ui_type"].GUI # You can change this to ENUMS["ui_type"].CONSOLE or ENUMS["ui_type"].GUI
 }
+
+def update_config(config):
+  """
+  Updates the configuration
+  :param config: Dictionary
+  :return: None
+  """
+  for key, value in config.items():
+    if key in CONFIG:
+      if isinstance(value, dict) and isinstance(CONFIG[key], dict):
+        update_config(value)
+      else:
+        CONFIG[key] = value
+
+def change_lang(lang):
+  """
+  Changes the language
+  :param lang: String
+  :return: None
+  """
+  CONFIG["lang"] = LANG[lang] if lang in LANG else LANG["en"]
