ce55a008e41cffa58cc3db4f314ef79967a211bb
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-03-05 22:57:37 +0700

    chore(main): add override config and make it oop

--- main.py
+++ main.py
@@ -1,4 +1,29 @@
-from converter import Base
+from handler.converter import Base
 
-if __name__ == "__main__": Base()
+class Main:
+  def __init__(self):
+    """
+    Base constructor
+    :return: None
+    """
+
+    self.__override()
+
+  def __override(self):
+    """
+    Override the base constructor
+    :return: None
+    """
+
+    # Make sure you update logic in converter handler
+    # bB default we only support override lang config
+    config = {
+      "lang": {
+        "default": "en"
+      }
+    }
+
+    Base(config)
+
+if __name__ == "__main__": Main()
 else: print("This is a module, not a standalone script.")
