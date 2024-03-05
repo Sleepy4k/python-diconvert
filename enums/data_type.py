faae941610f0c3d9e4599d1e808ff49defb61cd5
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2024-03-05 22:58:39 +0700

    chore(enum): add index for data type

--- enums/data_type.py
+++ enums/data_type.py
@@ -1,8 +1,8 @@
 from enum import Enum
 
 class DataType(Enum):
-  DECIMAL = 10
-  BINARY = 2
-  OCTAL = 8
-  HEXADECIMAL = 16
-  ASCII = 256
+  DECIMAL = (1, 10)
+  BINARY = (2, 2)
+  OCTAL = (3, 8)
+  HEXADECIMAL = (4, 16)
+  ASCII = (5, 256)
